87a127b7a8548cffee11e00f14e37fd7ca668514
Abdellahsyani <syaniabdos@gmail.com>
2025-02-26 16:31:40 +0000

    create client

---
+++ client.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asyani <asyani@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 12:44:44 by asyani            #+#    #+#             */
+/*   Updated: 2025/02/26 13:26:21 by asyani           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	ft_atoi(char *str)
+{
+	int			i;
+	int			sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+void	ft_convert_to_binary(int pid, int c)
+{
+	unsigned int	i;
+	unsigned int	bit;
+
+	i = 7;
+	while (i > 0)
+	{
+		bit = ((c >> i) & 1);
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i--;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	pid;
+
+	if (ac != 3 || av[2][0] == '\0')
+		return (1);
+	i = 0;
+	pid = ft_atoi(av[1]);
+	while (av[2][i])
+	{
+		ft_convert_to_binary(pid, av[2][i]);
+		i++;
+	}
+}
